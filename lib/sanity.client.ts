fe6c1f1b4671a008cf22399cb922ed7d65c629ed
= <mukherjee4004@gmail.com>
2023-02-09 21:10:12 +0530

    cdn use

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -3,9 +3,11 @@ export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION
 
+// use cdn is the better choice 
+// for not to overwhelmed the single point of api
 export const client = createClient({
     projectId,
     dataset,
     apiVersion,
-    useCdn: false
+    useCdn: true
 })
